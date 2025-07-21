5f5aa3827dd7697841414290ad94adbf97c3c81f
Jeff Klann <jklann@gmail.com>
2025-07-21 15:12:14 -0400

    DATA-51: RPDO table now has C_VISUALATTRIBUTES

--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/scripts/procedures/oracle/USP_RPDO2.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/scripts/procedures/oracle/USP_RPDO2.sql
@@ -97,7 +97,7 @@ BEGIN
       use_as_cohort
     FROM rpdo_table_request
     WHERE table_instance_id = p_table_instance_id
-    AND delete_flag = 'N';
+    AND DELETE_FLAG = 'N' AND C_VISUALATTRIBUTES LIKE '_A_' ;
 
     -- 3) Build the patient‐set SQL
     v_patientset_sql := udf_patientset_sql(
--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/scripts/procedures/postgresql/rpdo_patientset_sql.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/scripts/procedures/postgresql/rpdo_patientset_sql.sql
@@ -51,7 +51,7 @@ BEGIN
               FROM RPDO_TABLE_REQUEST
               WHERE USE_AS_COHORT = 'Y'
                 AND TABLE_INSTANCE_ID = p_table_instance_id
-                AND DELETE_FLAG = 'N'
+                AND DELETE_FLAG = 'N' AND C_VISUALATTRIBUTES LIKE '_A_' 
               ORDER BY SET_INDEX
             LOOP
                 v_c_tablename := rec.c_tablename;
