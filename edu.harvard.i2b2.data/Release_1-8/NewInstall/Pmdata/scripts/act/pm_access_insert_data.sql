afa3126759cf059a96747fc4d49adbd1b5737939
Michael Mendis <mmendis@partners.org>
2025-08-25 14:46:55 -0400

    defaultproject params for act

--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Pmdata/scripts/act/pm_access_insert_data.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Pmdata/scripts/act/pm_access_insert_data.sql
@@ -24,4 +24,29 @@ INSERT INTO PM_PROJECT_USER_ROLES (PROJECT_ID, USER_ID, USER_ROLE_CD, STATUS_CD)
 VALUES('ACT', 'act', 'EDITOR', 'A');
 INSERT INTO PM_PROJECT_USER_ROLES (PROJECT_ID, USER_ID, USER_ROLE_CD, STATUS_CD)
 VALUES('ACT', 'act', 'DATA_PROT', 'A');
+INSERT INTO PM_PROJECT_PARAMS (DATATYPE_CD, PROJECT_ID, PARAM_NAME_CD, VALUE, CHANGEBY_CHAR, STATUS_CD) VALUES ('T', 'ACT', 'Data Request Template', 'This user {{{USER_NAME}}} in project {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_MASTER_ID}}}.
+Check the status of the Data Request using the Data Request Manager plugi', 'i2b2', 'A');
+INSERT INTO PM_PROJECT_PARAMS (DATATYPE_CD, PROJECT_ID, PARAM_NAME_CD, VALUE,  CHANGEBY_CHAR, STATUS_CD) VALUES ('T', 'ACT', 'Data Request Email Address', 'mmendis@partners.org', 'i2b2', 'A');
+INSERT INTO PM_PROJECT_PARAMS (DATATYPE_CD, PROJECT_ID, PARAM_NAME_CD, VALUE, CHANGEBY_CHAR, STATUS_CD) VALUES ('T', 'ACT', 'Data Request Letter', '"Results of the i2b2 request entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, are available.
 
+Important notes about your data:
+	- Total number of patients returned in your data request: {{{PATIENT_COUNT}}}
+	- i2b2 reviewer:
+
+Only persons specifically authorized and selected (as listed at the top of this letter) can download these files. If additional user access is needed, please ensure the person is listed on your project IRB protocol and contact the i2b2 team.
+
+Specifically:
+	- Remove all PHI from computer, laptop, or mobile device after analysis is completed.
+	- Do NOT share PHI or PII with anyone who is not listed on the IRB protocol.
+
+Your guideline for the storage of Protected Health Information can be found at: https://www.site.com/guidelines_for_protecting_and_storing_phi.pdf
+
+*To download these files*
+- You must be logged onto your site
+
+These results are the data that was requested under the authority of the Institutional Review Board.  The query resulting in this identified patient data is included at the end of this letter.  A copy of this letter is kept on file and is available to the IRB in the event of an audit.
+
+Thank you,
+
+The i2b2 Team "', 'i2b2', 'A');
